01036a6453df7293fb6753462091a678f8093163
przemek <phocke@gmail.com>
2012-04-07 01:19:47 +0200

    Add all packages folders

---
+++ Packages/Sublime-HTMLPrettify/HTMLPrettify.py
@@ -0,0 +1,24 @@
+import commands, re
+import sublime, sublime_plugin
+
+class HtmlprettifyCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.save()
+    self.prettify(edit)
+
+  def save(self):
+    self.view.run_command("save")
+
+  def prettify(self, edit):
+    regx = re.compile(" ")
+    html = commands.getoutput("node " +
+      regx.sub("\ ", sublime.packages_path()) + "/HTMLPrettify/scripts/run.js " +
+      regx.sub("\ ", self.view.file_name()) +
+        " indent_size:\ 2" +
+        " indent_char:\ ' '" +
+        " max_char:\ 80" +
+        " brace_style:\ collapse")
+
+    if len(html) > 0:
+      self.view.replace(edit, sublime.Region(0, self.view.size()), html.decode('utf-8'))
+      sublime.set_timeout(self.save, 100)
---
+++ Packages/fredwu-ruby-slim.tmbundle/README.md
@@ -0,0 +1,15 @@
+# Slim TextMate Bundle
+
+## Description
+
+This is the [Textmate](http://macromates.com/) bundle for [Slim](http://slim-lang.com/), based on the [Handcrafted Haml bundle](http://github.com/handcrafted/handcrafted-haml-textmate-bundle).
+
+## Installation
+
+    cd ~/Library/Application\ Support/TextMate/Bundles/
+    git clone git://github.com/fredwu/ruby-slim-tmbundle.git Ruby-Slim.tmbundle
+    osascript -e 'tell app "TextMate" to reload bundles'
+
+## Author
+
+[Fred Wu](http://fredwu.me/)
